b56f2acccc097fce8d8e6aa3ff5ac93a4b83ebbb
Fabiano <fabiano.chagas@gmail.com>
2020-03-16 11:59:22 -0400

    Adding the terraform scripts for Suriname

--- README.md
+++ README.md
@@ -19,9 +19,10 @@
 1. Clone the repo
 2. cd terraform_sogema
 3. go to the directory you have interest on
-3. az login 
-3. terraforme init
-4. terraform plan
-5. terraform apply
+4. az login
+  - for security purposes, we decided to not add the subscription_id in the terraform scripts. The analyst needs to login manually prior to launch the terraform. 
+5. terraforme init
+6. terraform plan
+7. terraform apply
 
 ---
---
+++ eFiling/qa-sr/qa-sr-efi-mdb01/qa-sr-efi-mdb01.tf
@@ -0,0 +1,114 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  version         = ">= 2.0"
+  features {}
+}
+
+variable "admin_username" {
+  default = "efi"
+}
+
+variable "admin_password" {
+  default = "S4b4d0@w0rk#01"
+}
+
+variable "environment_prefix" {
+  default = "qa-sr"
+}
+
+variable "environment_tag" {
+  default = "eFiling QA Suriname"
+}
+
+variable "product_tag" {
+  default = "eFiling"
+}
+
+variable "role_tag" {
+  default = "qa"
+}
+
+# You'll usually want to set this to a region near you.
+variable "location" {
+  default = "eastus2"
+}
+
+#####################################################################################################
+#refer to an existing vnet
+data "azurerm_virtual_network" "existing_virtualnet" {
+  name                 = "RG_EastUS2_Demo-vnet2"
+  resource_group_name  = "RG_EastUS2_Demo"
+}
+#refer to an existing subnet
+data "azurerm_subnet" "existing_subnet" {
+  name                 = "Subnetwork2"
+  virtual_network_name = "RG_EastUS2_Demo-vnet2"
+  resource_group_name  = "RG_EastUS2_Demo"
+}
+# Imported resource group
+data "azurerm_resource_group" "existing_resource_group" {
+  name     = "RG_EastUS2_Demo"
+}
+
+#####################################################################################################
+# This is for the load balancer box
+resource "azurerm_network_interface" "new_terraform_mdb01_nic01" {
+    name                      = "${var.environment_prefix}-efi-mdb01_nic01"
+    resource_group_name       = data.azurerm_resource_group.existing_resource_group.name
+    location                  = data.azurerm_resource_group.existing_resource_group.location
+
+    ip_configuration {
+        name                           = "mdb01-nic01_conf"
+        subnet_id                      = data.azurerm_subnet.existing_subnet.id
+        private_ip_address_allocation  = "Dynamic"
+    }
+
+    tags = {
+        environment = "${var.environment_tag}"
+        product = "${var.product_tag}"
+        role = "${var.role_tag}"
+    }
+}
+
+#########################################################################################
+#VM Creation
+resource "azurerm_virtual_machine" "new_terraform_mdb01" {
+    name                  = "${var.environment_prefix}-efi-mdb01"
+    location              = "eastus2"
+    resource_group_name   = data.azurerm_resource_group.existing_resource_group.name
+    network_interface_ids = ["${azurerm_network_interface.new_terraform_mdb01_nic01.id}"]
+    vm_size               = "Standard_B2ms"
+
+    storage_os_disk {
+        name              = "${var.environment_prefix}-efi-mdb01_osDisk"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Standard_LRS"
+    }
+
+    delete_os_disk_on_termination = true
+    
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "${var.environment_prefix}-efi-mdb01"
+        admin_username = var.admin_username
+	    admin_password = var.admin_password
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+
+    tags = {
+        environment = "${var.environment_tag}"
+        product = "${var.product_tag}"
+        role = "${var.role_tag}"
+    }
+}
+
